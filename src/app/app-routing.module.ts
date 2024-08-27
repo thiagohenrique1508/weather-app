1b854e003fd4694df7b12c027fe73435df04d997
Thiago Henrique <thiagohenrique1508@gmail.com>
2024-08-26 21:53:19 -0300

    Criação do componente wheater-home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WheaterHomeComponent } from './modules/wheater/page/wheater-home/wheater-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'wheater',
+    pathMatch: 'full'
+  },
+  {
+    path:'wheater',
+    component: WheaterHomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WheaterHomeComponent } from './modules/wheater/page/wheater-home/wheater-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WheaterHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
